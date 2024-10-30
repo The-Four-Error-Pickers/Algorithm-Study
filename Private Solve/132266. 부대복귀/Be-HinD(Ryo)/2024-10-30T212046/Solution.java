3ae87d1a17da7b458f60501ef02167d19a8096ae
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2024-10-30 21:20:46 +0900

    [docs] Be-HinD(Ryo) 부대복귀 풀이 코드 2024-10-30T212046

---
+++ Private Solve/132266. 부대복귀/Be-HinD(Ryo)/2024-10-30T212046/Solution.java
@@ -0,0 +1,60 @@
+import java.util.*;
+class Solution {
+    static List<List<Integer>> list;
+    public int[] solution(int n, int[][] roads, int[] sources, int destination) {
+        /**
+         각 지역은 유일한 번호로 구분
+         걸리는 시간은 모두 1로 동일
+         최단시간에 부대로 복귀
+         임무의 시작 때와 다르게 되돌아오는 경로가 없어져 복귀가 불가능한 부대원도 있을 수 있습니다.
+         
+         **/
+        
+        list = new ArrayList<>();
+        for(int i=0; i<n; i++) list.add(new ArrayList<>());
+        
+        for(int i=0; i<roads.length; i++) {
+            int[] road = roads[i];
+            list.get(road[0]-1).add(road[1]-1);
+            list.get(road[1]-1).add(road[0]-1);
+        }
+        
+        List<Integer> res = new ArrayList<>();
+        for(int i=0; i<sources.length; i++) {
+            res.add(dijk(n, sources[i]-1, destination-1));
+        }
+        
+        int[] ress = new int[res.size()];
+        for(int i=0; i<res.size(); i++) {
+            ress[i] = res.get(i);
+        }
+        return ress;
+    }
+    
+    private static int dijk(int n, int start, int target) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2) -> o1[1] - o2[1]);
+        boolean[] v = new boolean[n];
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        
+        pq.offer(new int[]{start,0});
+        dist[start] = 0;
+        
+        while(!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            
+//             if(v[cur[0]]) continue;
+            
+            
+            for(int next : list.get(cur[0])) {
+                if(!v[next] && dist[next] > cur[1]+1) {
+                    pq.offer(new int[]{next, cur[1]+1});
+                    dist[next] = cur[1]+1;
+                    v[cur[0]] = true;
+                }
+            }
+        }
+        
+        return dist[target] == Integer.MAX_VALUE ? -1 : dist[target];
+    }
+}
