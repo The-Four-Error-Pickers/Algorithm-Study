7862c21aecabb3623a28f1abb07caa4a9e8f52ee
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-10-30 21:26:03 +0900

    [docs] ChaNyeok1225 부대복귀 풀이 코드 2024-10-30T212622

---
+++ Private Solve/132266. 부대복귀/ChaNyeok1225/2024-10-30T212622/Solution.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int[][] roads, int[] sources, int destination) {
+        int len = sources.length;
+        int[] answer = new int[len];
+        
+        ArrayList<Integer>[] graph = new ArrayList[n+1];
+        for(int i = 1; i < n + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        for(int[] edge : roads) {
+            graph[edge[1]].add(edge[0]);
+            graph[edge[0]].add(edge[1]);
+        }
+            
+        ArrayDeque<Integer> q = new ArrayDeque<>();            
+        
+        int[] cost = new int[n+1];
+        Arrays.fill(cost, -1);
+        
+        q.offer(destination);
+        cost[destination] = 0;
+        
+        int cur, size, step = 0;
+        while(!q.isEmpty()) {
+            
+            size = q.size();
+            step++;
+            for(int i = 0; i < size; i++) {
+                cur = q.poll();
+                for(int nxt : graph[cur]) {
+                    if(cost[nxt] != -1) continue;
+                    cost[nxt] = step;
+                    q.offer(nxt);
+                }
+            }
+        }        
+        
+        System.out.println(Arrays.toString(cost));
+        
+        for(int i = 0; i < len; i++) {
+            answer[i] = cost[sources[i]];
+        }
+        
+        return answer;
+    }
+}
