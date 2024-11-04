d71e17f5bfa05c5f707ce65e9590c9359ddf2110
JeongHyeon Kim <78507813+Be-HinD@users.noreply.github.com>
2024-11-04 20:52:45 +0900

    [docs] Be-HinD(Ryo) 선입 선출 스케줄링 풀이 코드 2024-11-4T205245

---
+++ Private Solve/12920. 선입 선출 스케줄링/Be-HinD(Ryo)/2024-11-4T205245/Solution.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[] cores) {
+        int answer = 0;
+    
+        int low = 0;
+        int high = Integer.MAX_VALUE;
+        int task = 0;
+        int time = 0;
+        while(low <= high) {
+            final int mid = low + (high-low)/2;
+            
+            int sum = 0;
+            for(int i=0; i<cores.length; i++) {
+                sum += mid / cores[i];
+                if(sum >= n) break;
+            }
+            
+            if(sum >= n) {
+                high = mid - 1;
+                time = mid;
+                task = sum;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        
+        task -= n; //남은 작업
+        
+        for(int i=cores.length-1; i>=0; i--) {
+            if(time % cores[i] == 0) {
+                if(task == 0) {
+                    return i+1;
+                }
+                task--;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
