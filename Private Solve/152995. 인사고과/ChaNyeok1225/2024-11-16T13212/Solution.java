939b196f1c2e43bf271f8ae7a08a378e99ffdc00
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-11-16 13:20:58 +0900

    [docs] ChaNyeok1225 인사고과 풀이 코드 2024-11-16T13212

---
+++ Private Solve/152995. 인사고과/ChaNyeok1225/2024-11-16T13212/Solution.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    
+    public int solution(int[][] scores) {
+        int n = scores.length;
+        
+        int[] wonho = {scores[0][0], scores[0][1]};
+        
+        int MAX_VAL = 100_001;
+        int[] acc = new int[MAX_VAL];
+        
+        for(int i = 0; i < n; i++) {
+            acc[scores[i][0]] = Math.max(acc[scores[i][0]], scores[i][1]);
+        }
+        for(int i = MAX_VAL - 2; i >= 0; i--) {
+            acc[i] = Math.max(acc[i], acc[i+1]);
+        }
+        
+        Arrays.sort(scores, (a,b) -> {
+            return -Integer.compare(a[0]+a[1], b[0]+b[1]);
+        });
+        
+        int grade = 0;
+        int cnt = 0;
+        int prev = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i++) {
+            if(scores[i][1] < acc[scores[i][0]])
+                continue;
+            cnt++;
+            
+            if(prev > scores[i][0] + scores[i][1]) {
+                grade += cnt;
+                cnt = 0;
+                prev = scores[i][0] + scores[i][1];
+            }
+            if(wonho[0] == scores[i][0] && wonho[1] == scores[i][1])
+                return grade;
+        }
+        
+        return -1;
+    }
+}
