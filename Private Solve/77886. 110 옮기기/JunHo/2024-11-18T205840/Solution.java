21743bacd79d1c92553db0c487ce129079a7f8ee
hoing97s <140311409+hoing97s@users.noreply.github.com>
2024-11-18 20:58:40 +0900

    [docs] JunHo 110 옮기기 풀이 코드 2024-11-18T205840

---
+++ Private Solve/77886. 110 옮기기/JunHo/2024-11-18T205840/Solution.java
@@ -0,0 +1,51 @@
+class Solution {
+    public String[] solution(String[] s) {
+        String[] answer = new String[s.length];
+        
+        for(int i = 0; i < s.length; i++) {
+            char[] word = new char[s[i].length()];
+            int idx = 0;
+            int count = 0;
+            
+            for(int j = 0; j < word.length; j++) {
+                if((s[i].charAt(j) == '0') && (idx >= 2) && 
+                   (s[i].charAt(idx - 1) == '1') && (s[i].charAt(idx - 2) == '1')) {
+                    idx -= 2;
+                    count++;
+                } else {
+                    word[idx++] = s[i].charAt(j);
+                }
+            }
+            
+            // for(int j = 0; j < idx; j++) {
+            //     System.out.print(word[j] + " ");
+            // }
+            // System.out.println();
+            
+            StringBuilder sb = new StringBuilder();
+            for(int j = 0; j < idx; j++) {
+                sb.append(word[j]);
+            }
+            
+            StringBuilder temp = new StringBuilder();
+            for(int j = 0; j < count; j++) {
+                temp.append("110");
+            }
+            
+            int zeroIndex = sb.lastIndexOf("0");
+          
+            if(zeroIndex < 0) {
+                sb.insert(0, temp.toString());
+            }
+            else {
+                sb.insert(zeroIndex + 1, temp.toString());
+            }
+            
+            
+            
+            answer[i] = sb.toString();
+        }
+        
+        return answer;
+    }
+}
