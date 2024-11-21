b04d6ec630f9e4f6298e924efa541efffc756222
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-11-21 21:41:51 +0900

    [docs] ChaNyeok1225 카운트 다운 풀이 코드 2024-11-21T214150

---
+++ Private Solve/131129. 카운트 다운/ChaNyeok1225/2024-11-21T214150/Solution.java
@@ -0,0 +1,38 @@
+// DP 풀이
+
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int target) {
+        int[] answer = new int[2];
+        
+        int[][] dp = new int[target+100][2];
+        int num;
+        for(int i = 0; i < target; i++) {
+            
+            num = i + 50;
+            if(dp[num][0] == 0) {
+                dp[num][0] = dp[i][0] + 1;
+                dp[num][1] = dp[i][1] + 1;
+            } else if(dp[num][0] == dp[i][0] + 1){
+                dp[num][1] = Math.max(dp[num][1], dp[i][1] + 1);
+            }
+            
+            for(int m = 1; m < 4; m++) {
+                for(int s = 1; s < 21; s++) {
+                    num = i + m * s;
+                    
+                    if(dp[num][0] == 0) {
+                        dp[num][0] = dp[i][0] + 1;
+                        dp[num][1] = dp[i][1] + (m == 1? 1 : 0);
+                    } else if(dp[num][0] == dp[i][0] + 1){
+                        dp[num][1] = Math.max(dp[num][1], dp[i][1] + (m == 1? 1 : 0));
+                    }
+                }
+            }
+        }
+        
+        return new int[] {dp[target][0], dp[target][1]};
+    }
+}
